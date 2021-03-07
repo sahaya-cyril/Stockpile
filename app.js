7d97cd2449c316b66a0bdc32837ece20c2fda770
Cyril <sahayacyril2015@gmail.com>
2021-03-08 03:36:17 +0530

    Add item form

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+app.get("/AddItem", (req, res) => {
+    res.render("AddItem");
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
