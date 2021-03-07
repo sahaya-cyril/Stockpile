bb6be908e517e25cc76036b46f4ddc71178d7d42
Cyril <sahayacyril2015@gmail.com>
2021-03-08 02:12:06 +0530

    Complete frontend

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
